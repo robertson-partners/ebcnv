acc82aa314afc694368c02e431022d9292ce0b03
Tom Madrid <madrid1979@gmail.com>
2019-01-08 14:48:44 -0800

    Updated VV test

--- template-full-width-test.php
+++ template-full-width-test.php
@@ -106,73 +106,10 @@ get_header(); ?>
       <div class="grid-container">
         <div class="grid-x grid-padding-x">
           <div class="medium-12 large-12 cell">
-
+          <h2>send a <b>letter</b></h2>
             <!-- VOTER VOICE TEST -->
             <script type="text/javascript" src="https://www.votervoice.net/Scripts/Cg0AAAAAAAA/Plugin.js?app=campaigns"></script><vv:main></vv:main>
             <!-- END VOTER VOICE -->
-
-            <!--
-            <h2>send a <b>letter</b></h2>
-            <p>We've put together a letter. You can edit it to add any personal sentiments you wish. Click "Submit" below the letter to send it to your state and local representatives.</p>
-            <div class="letterbox">
-              <div class="letterreset" id="letterreset"><i class="fal fa-redo"></i> start over</div>
-              <div class="instructions"><i class="fal fa-pencil"></i> Edit the letter by clicking/tapping below</div>
-              <hr>
-              <p class="hidden-date"><?php echo date('F jS, Y'); ?></p>
-              <div class="editabletxt" id="letterbox" contenteditable="true">
-                <p><span>To whom it may concern,</span></p>
-                <p><span>The critical, life-saving and advanced care many of Nevada’s smallest and sickest citizens receive at Sunrise Hospital’s Neonatal Intensive Care Unit and other pediatric departments is important to our state. As the only hospital in Nevada fully equipped to treat our state’s sickest and smallest babies and the state’s largest provider of Medicaid services, Sunrise Hospital plays an important role in the health of some Nevada families.</span></p>
-                <p><span>To ensure these critical services can continue to be provided to families, children and babies in Nevada, I ask for your support as the hospital seeks a fair and equitable distribution of Medicaid reimbursements commensurate with the services it provides. It matters to me that Nevada’s sickest babies and children continue to receive the critical, life-saving care they need to survive and thrive.</span></p>
-                <p class="hide-on-print"><span>(Write your own words here or anywhere throughout this letter.)</span></p>
-                <p><span>Signed,</span></p>
-              </div>
-
-              <!-- LETTER FORM ->
-              <div class="letter-requirements" id="letter-requirements">
-                <form class="ltr-sender" id="ltr-sender">
-                  <input type="hidden" name="g-recaptcha-response" id="gkey" value="">
-                  <input type="hidden" name="letter-content-text" id="letter-content-text" value="">
-                  <div class="fields grid-x grid-padding-x">
-                    <div class="small-12 medium-4 large-4 cell">
-                      <label for="fname_letter">Your First Name<sup>*</sup></label>
-                      <input type="text" id="fname_letter" name="fname_letter">
-                    </div>
-                    <div class="small-12 medium-4 large-4 cell">
-                      <label for="lname_letter">Your Last Name<sup>*</sup></label>
-                      <input type="text" id="lname_letter" name="lname_letter">
-                    </div>
-                    <div class="small-12 medium-4 large-4 cell">
-                      <label for="email_letter">Your Email Address<sup>*</sup></label>
-                      <input type="email" id="email_letter" name="email_letter">
-                    </div>
-                  </div>
-                  <div class="fields grid-x grid-padding-x">
-                    <div class="small-12 medium-12 large-12 cell letter-req">
-                      <sup>*</sup> Denotes a required field
-                    </div>
-                  </div>
-                  <div class="g-recaptcha" id="g-recaptcha"></div>
-                  <div class="submit_letter grid-x grid-padding-x" id="submit_letter">
-                    <div class="small-12 medium-4 large-4 cell">
-                      <div class="submit_btn_letter inactive" data-state="inactive" id="ltr_submit"><i class="fal fa-envelope-open"></i> submit your letter</div>
-                    </div>
-                    <div class="small-12 medium-8 large-8 cell acknowledgment">
-                      <p>By clicking "submit your letter", you acknowledge that your letter is non-commercial, contains respectful content and is appropriate to be delivered to your state/local respresentative of Nevada.</p>
-                    </div>
-                  </div>
-                </form>
-              </div>
-              <!-- LETTER FORM -->
-
-              <div class="completed-letter" id="completed_letter">
-                <p class="yourname" id="your_name_completed"></p>
-                <hr>
-                <p class="thank-you">Thank you for adding your voice. Together we can ensure care for every baby in Southern Nevada.</p>
-              </div>
-
-            </div>
-            <div class="printtxt" id="printtxt">
-              <i class="fal fa-print"></i> print letter
             </div>
           </div>
         </div>
